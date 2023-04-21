afe19a57e0bfc0f4ad505a9f80c8feeb01ddb427
CongSinh <sinhkute9xxx@gmail.com>
2023-04-21 22:22:54 +0700

    test

--- src/components/Sidebar/RightSidebar.vue
+++ src/components/Sidebar/RightSidebar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed right-[10rem]  top-14 mt-6">
+  <div class="fixed  right-[10rem]  top-14 mt-6">
     <!-- Title  -->
     <TitleSidebar title="Xu hướng" src="@/assets/img/AvatarLoading.png" />
     <!-- Title  -->
