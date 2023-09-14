0a659be964f86ba9221668c18d16efa103c97f2c
Macbook <macbook@MacBook-Pro-cua-Sinhdz.local>
2023-09-14 13:20:43 +0700

    fix comment

--- src/pages/Comment/CommentErorr.vue
+++ src/pages/Comment/CommentErorr.vue
@@ -1,20 +1,21 @@
 <template>
-    <div class="pb-8 pt-3 opacity-[50%]">
-        <div class="flex justify-between items-center">
-            <AvatarWithNameAndTime name="Phạm Đăng Phúc" time="15 phút trước" />
-            <img srcset="@/assets/img/seeMore.png 2x" alt="">
-        </div>
-        <content-comment-detail
-            comment="Ông Dũng cho biết qua báo cáo của các địa
+  <div class="pb-8 pt-3 opacity-[50%]">
+    <div class="flex items-center justify-between">
+      <AvatarWithNameAndTime name="Phạm Đăng Phúc" time="15 phút trước" />
+      <img srcset="@/assets/img/seeMore.png 2x" alt="" />
+    </div>
+    <content-comment-detail
+      comment="Ông Dũng cho biết qua báo cáo của các địa
                                                                         phương, các hiệp hội cho thấy thị
                                                                         trường bất động sản cao cấp được đầu tư nhiều trong khi đó nhà ở xã hội, nhà có giá phù hợp rất
-                                                                        hạn chế. Đây là biểu hiện lệch pha rõ nét ở các đô thị đặc th..." />
+                                                                        hạn chế. Đây là biểu hiện lệch pha rõ nét ở các đô thị đặc th..."
+    />
 
-        <div class="flex items-center">
-            <voted vote="278" class=" w-7" />
-            <p class="text-S font-medium text-grey6 ml-[5rem]">Phản hồi</p>
-        </div>
+    <div class="flex items-center">
+      <voted vote="278" class="w-7" />
+      <p class="text-S font-medium text-grey6 ml-[5rem]">Phản hồi</p>
     </div>
+  </div>
 </template>
 
 <script>
@@ -23,8 +24,8 @@ import ContentCommentDetail from './structureComment/ContentComment.vue';
 import Voted from '@/components/ArticleCard/CardIngredient/Voted.vue';
 
 export default {
-    components: { AvatarWithNameAndTime, ContentCommentDetail, Voted }
-}
+  components: { AvatarWithNameAndTime, ContentCommentDetail, Voted },
+};
 </script>
 
 <style></style>
--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -1,73 +1,75 @@
 <template>
   <Header class="max-[768px]:hidden" />
 
-  <div class="grid grid-cols-12 bg-background1 font-font pt-14 max-[768px]:pt-0 max-[768px]:grid-cols-1 ">
-
+  <div class="grid grid-cols-12 bg-background1 font-font pt-14 max-[768px]:pt-0 max-[768px]:grid-cols-1">
     <LeftDetail class="max-[768px]:hidden" />
 
-    <main class="font-font col-span-5 col-start-4 mt-140 mb-7 ">
+    <main class="col-span-5 col-start-4 font-font mt-140 mb-7">
       <router-link to="/">
         <IconLogoDetail :title="articleDetail?.categories?.[0]?.name" />
       </router-link>
-      <div class="bg-white pt-4 px-3 rounded-lg">
-        <AvatarWithNameAndTime :src="articleDetail?.publishedBy?.avatar.url" :name="articleDetail?.publishedBy?.fullname"
-          time="22 phút trước" class="ml-2 text-M" />
+      <div class="px-3 pt-4 bg-white rounded-lg">
+        <AvatarWithNameAndTime
+          :src="articleDetail?.publishedBy?.avatar.url"
+          :name="articleDetail?.publishedBy?.fullname"
+          time="22 phút trước"
+          class="ml-2 text-M"
+        />
 
         <TitleDetail :title="articleDetail?.comment" class="text-sm" />
         <!-- bài báo -->
 
-
         <!-- <Article></Article> -->
 
         <!-- bài báo -->
-        <div class="bg-grey0 rounded p-4">
-
+        <div class="p-4 rounded bg-grey0">
           <newspaperLogo :title="articleDetail?.source?.domain" :src="articleDetail?.source?.logo" />
           <div class="font-semibold text-heading6 text-background4 t-4" v-html="articleDetail?.title"></div>
 
           <Summary :summary="articleDetail?.summary" />
 
-          <div class=" pt-3 text-M text-grey8 font-normal   " v-html="articleDetail?.description" />
+          <div class="pt-3 font-normal text-M text-grey8" v-html="articleDetail?.description" />
         </div>
         <!-- bài báo -->
-
 
         <Share />
 
         <TrendNews :items="articleDetail?.feedsRelated" />
-
 
         <!-- comment post  -->
         <div>
           <div class="flex justify-between pt-[2rem] pb-[1rem]">
             <h3 class="font-semibold text-text1 text-grey8">Bình luận (12)</h3>
             <ul class="flex items-center">
-              <p class="pr-2 relative text-background4 text-text2 font-normal cursor-pointer">Mới nhất</p>
+              <p class="relative pr-2 font-normal cursor-pointer text-background4 text-text2">Mới nhất</p>
               <img srcset="@/assets/img/IconUserDown.png 2x" alt class="items-center" />
               <ul class="rounded-lg absolute mt-[8rem] ml-[-3rem] z-30 bg-white shadow-2xl">
-                <li class="ml-4 pr-12 my-5 text-background4 text-text2 font-normal">Mới nhất</li>
-                <li class="ml-4 pr-12 my-5 text-background4 text-text2 font-normal">Cũ nhất</li>
+                <li class="pr-12 my-5 ml-4 font-normal text-background4 text-text2">Mới nhất</li>
+                <li class="pr-12 my-5 ml-4 font-normal text-background4 text-text2">Cũ nhất</li>
               </ul>
             </ul>
           </div>
 
-          <input type="text" placeholder="Bình luận của bạn..."
-            class="h-11 w-full border p-7 outline-none rounded-lg my-4" />
+          <input
+            type="text"
+            placeholder="Bình luận của bạn..."
+            class="w-full my-4 border rounded-lg outline-none h-11 p-7"
+          />
 
           <!-- comment post  -->
 
-
           <!-- comment -->
-          <CommentErorr />
+          <!-- <CommentErorr />
           <CommentDetail />
           <RepComment />
           <CommentDetail />
-          <CommentDetail />
+          <CommentDetail /> -->
           <!-- comment -->
 
           <div class="flex justify-center py-8">
             <button
-              class="text-text4 font-medium justify-center flex items-center mx-4 w-[7rem] h-7 text-grey7 border border-grey1 rounded-[44px]">
+              class="text-text4 font-medium justify-center flex items-center mx-4 w-[7rem] h-7 text-grey7 border border-grey1 rounded-[44px]"
+            >
               Xem Thêm
               <img srcset="@/assets/img/IconUserDown.png 2x" alt class="pl-2" />
             </button>
@@ -76,7 +78,6 @@
       </div>
     </main>
     <RightSidebar class="mt-[4.5rem] font-semibold" />
-
   </div>
 </template>
 
@@ -95,12 +96,11 @@ import RepComment from '@/pages/Comment/RepComment.vue';
 import CommentErorr from '@/pages/Comment/CommentErorr.vue';
 import IconLogoDetail from './componentsDetail/IconLogoDetail.vue';
 import newspaperLogo from './componentsDetail/newspaperLogo.vue';
-import TitleDetail from '@/pages/Detail/TitleDetail/TitleDetail.vue'
+import TitleDetail from '@/pages/Detail/TitleDetail/TitleDetail.vue';
 import { isFeedId } from '@/utils/number';
 import { mapActions, mapGetters } from 'vuex';
 
 export default {
-
   components: {
     Header,
     RightSidebar,
@@ -116,15 +116,14 @@ export default {
     CommentErorr,
     IconLogoDetail,
     newspaperLogo,
-    TitleDetail
+    TitleDetail,
   },
   created() {
     const currentFeedId = this.$route && this.$route.params?.id ? this.$route.params?.id : null;
-    this.getFeedDetail(currentFeedId)
+    this.getFeedDetail(currentFeedId);
   },
 
-  mounted() {
-  },
+  mounted() {},
   computed: {
     ...mapGetters('feed', {
       articleDetail: 'articleDetail',
@@ -142,7 +141,7 @@ export default {
       next('/404');
     }
   },
-}
+};
 </script>
 
 <style></style>
